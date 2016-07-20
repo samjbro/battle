acd408e6d07d14a97e670158b8b7f4b0cf144634
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-07-20 21:11:05 +0100

    Refacotered code including html structure of views

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,16 @@
 class Game
-  attr_reader :player_1, :player_2
+  #attr_reader :player_1, :player_2
 
   def initialize(player_1, player_2)
-    @player_1 = player_1
-    @player_2 = player_2
+    @players = [player_1, player_2]
+  end
+
+  def player_1
+    @players.first
+  end
+
+  def player_2
+    @players.last
   end
 
   def attack(opponent)
