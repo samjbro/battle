3120d51b48268b7c9db2c932e88a7563d5477fe6
cyberplanner <saleh.gfx@gmail.com>
2016-07-20 17:06:46 +0100

    Step 26: Skinny Controllers, tests passed.

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,22 @@
 require_relative 'player'
 
 class Game
-
-	def attack player2
-		player2.reduce_hp
+	attr_reader :players
+	
+	def initialize player_1:, player_2:
+		@players = [player_1, player_2]
 	end
+
+	def attack player
+		player.reduce_hp
+	end
+
+	def player1
+		@players[0]
+	end
+
+	def player2
+		@players[1]
+	end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,12 @@ require 'game'
 describe Game do
 	let(:player_1) { double :player_1 }
 	let(:player_2) { double :player_2 }
-	subject(:game) {described_class.new}
+	subject(:game) {described_class.new(player_1: player_1, player_2: player_2)}
+
+	it "a new game takes two players" do
+		expect(game.player1).to eq player_1
+		expect(game.player2).to eq player_2
+	end
 
 	context "#attack" do
 		it "attacks another player" do
@@ -11,5 +16,5 @@ describe Game do
 			game.attack(player_2)
 		end
 	end
-	
+
 end
