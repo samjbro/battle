b2599878d7f260e738e9fefbe62e2431fb12d667
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-07-20 19:30:49 +0100

    Refactored controller to make it lighter by moving players into game

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,9 +3,9 @@
 feature 'Enter names' do
   scenario 'submitting names' do
     visit('/')
-    fill_in :player_1_name, with: 'Jess'
-    fill_in :player_2_name, with: 'Sam'
+    fill_in :player_1_name, with: 'Player 1'
+    fill_in :player_2_name, with: 'Player 2'
     click_button 'Submit'
-    expect(page).to have_content 'Jess vs. Sam'
+    expect(page).to have_content 'Player 1 vs. Player 2'
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,14 +2,24 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new }
-  let(:mannie) { double('Mannie') }
-  let(:jess) { double('Player') }
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double('Player') }
+  let(:player_2) { double('Player') }
 
   describe '#attack' do
     it 'damages your opponent' do
-      expect(jess).to receive(:receive_damage)
-      game.attack(jess)
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
+    end
+  end
+
+  context 'When in the game' do
+    it 'Player_1 equals player 1' do
+      expect(game.player_1).to eq player_1
+    end
+
+    it 'Player_2 equals player 2' do
+      expect(game.player_2).to eq player_2
     end
   end
 
