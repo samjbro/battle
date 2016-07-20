c2429a9c48b35bafd92b883b321e1228689482fb
samjbro <sjbroughton1@gmail.com>
2016-07-20 12:50:40 +0100

    Ch20: Implements user story 2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -3,9 +3,19 @@ require "spec_helper"
 feature "Enter names" do
   scenario 'submitting names' do
     visit '/'
-    fill_in :player_1_name, with: "Dave"
-    fill_in :player_2_name, with: "Mittens"
+    fill_in :player_1_name, with: "Sam"
+    fill_in :player_2_name, with: "Sal"
     click_button "Submit"
-    expect(page).to have_content "Dave vs. Mittens"
+    expect(page).to have_content "Sam vs. Sal"
   end
+end
+
+feature "Hit points" do
+	scenario "seeing opponent's hitpoints" do
+		visit '/'
+		fill_in :player_1_name, with: "Sam"
+    fill_in :player_2_name, with: "Sal"
+    click_button "Submit"
+		expect(page).to have_content 'Sal: 100 hp'
+	end
 end
