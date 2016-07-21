59534e41a35534a55f5874854723e55b0a2a19b3
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-21 16:08:06 +0100

    Ex22 player 1 can attack player 2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,4 +1,4 @@
-feature 'enter names' do
+feature 'Enter names' do
   scenario 'allows users to enter and view names' do
     sign_in_and_play
     expect(page).to have_content "Richard vs. Jonny!"
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,4 +1,4 @@
-feature 'hit points' do
+feature 'Hit points' do
   scenario "we can see player 1 and 2's hit points" do
     sign_in_and_play
     expect(page).to have_content "Jonny HP:100" && "Richard HP:100"
