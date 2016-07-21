8e3f2caa33294c3ec9c07d795b291bfc84957f14
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-21 22:40:47 +0100

    Ex 26: refactors using @game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,14 +2,27 @@ require "game"
 require "player"
 
 describe Game do
-  subject(:game){Game.new}
-  subject(:player_1){Player.new("Bob")}
-  subject(:player_2){Player.new("Bill")}
+  subject(:game){described_class.new(player_1, player_2)}
+  subject(:player_1){double :player}
+  subject(:player_2){double :player}
+
+  describe '#player1' do
+    it 'accepts the first player' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe '#player_2' do
+    it 'accepts the second player' do
+      expect(game.player_2).to eq player_2
+    end
+  end
+
   describe '#attack' do
     it 'damages the player' do
       expect(player_2).to receive(:receive_damage)
       game.attack(player_2)
-      #WHY?????????????????????????????????????????     
+      #WHY?????????????????????????????????????????
     end
   end
 end
