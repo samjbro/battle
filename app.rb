93d5fa1ed370245e15f229edd61bb150cd7ee870
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-07-21 15:25:51 +0100

    Storing Game object within self, Completed challenge 30

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ require './lib/game.rb'
 class Battle < Sinatra::Base
   enable :sessions
 
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb :index
   end
@@ -13,17 +17,15 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(player_1, player_2)
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @game = $game
     @game.attack(@game.defending_player)
     @game.switcher
     erb :attack
