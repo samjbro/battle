3fdcbf405ccfbd6706a2ea7e716b98471e785441
Richard Bates <richo225@gmail.com>
2016-07-21 19:26:43 +0100

    Ex25 Extract game class

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,16 @@
+require_relative "game.rb"
+
 class Player
 
 attr_reader :name, :hp
 
-def initialize(name)
-  @name = name
-  @hp = 100
-end
+  def initialize(name)
+    @name = name
+    @hp = 100
+  end
 
-def attack
-  @hp -= 10
-end
+  def receive_damage
+    @hp -= 10
+  end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,10 +10,4 @@ describe Player do
   it 'starts with 100 HP' do
     expect(player_1.hp).to eq 100
   end
-
-  describe '#attack' do
-    it 'reduces your own HP by 10' do
-      expect{player_2.attack}.to change{player_2.hp}.by (-10)
-    end
-  end
 end
