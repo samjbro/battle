c79a3caebbff8ae8923d6fc8fddb3bd63d120030
samjbro <sjbroughton1@gmail.com>
2016-07-22 12:42:15 +0100

    Automates player 2's turn

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -3,10 +3,23 @@ feature 'Play' do
 		sign_in_and_play
 		expect(page).to have_content "Sam's turn!"
 	end
-	scenario "shows that it is player 2's turn after player 1 has attacked" do
+
+	scenario "displays 'Attack!' button on player 1's turn" do
+		sign_in_and_play
+		expect(page).to have_button "Attack!"	
+	end
+
+	before do
 		sign_in_and_play
 		click_button("Attack!")
     click_button("Resume combat!")
+	end
+
+	scenario "shows that it is player 2's turn after player 1 has attacked" do
 		expect(page).to have_content "Jonny's turn!"
+	end
+
+	scenario "displays 'Continue' button on player 2's turn" do
+    expect(page).to have_button "Continue"	
 	end
 end
